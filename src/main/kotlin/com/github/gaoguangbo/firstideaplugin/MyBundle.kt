cacc4a17f5c996d7a6e8f46dee3c587bd4c62139
GitHub Action <action@github.com>
2021-12-16 05:10:49 +0000

    Template cleanup

--- src/main/kotlin/com/github/gaoguangbo/firstideaplugin/MyBundle.kt
+++ src/main/kotlin/com/github/gaoguangbo/firstideaplugin/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.gaoguangbo.firstideaplugin
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
---
+++ src/main/kotlin/com/github/gaoguangbo/firstideaplugin/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.gaoguangbo.firstideaplugin.services
+
+import com.github.gaoguangbo.firstideaplugin.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
